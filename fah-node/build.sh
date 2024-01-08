19f29666d65e904ddbe99c5204fdb15a2af28fdb
Dmytro Prokhorenkov <liet@liet.kiev.ua>
2024-01-08 22:24:55 +0100

    Updated fah-node dockerimage

--- fah-node/build.sh
+++ fah-node/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y scons build-essential libssl-dev binutils-dev libleveldb-dev libsnappy-dev git nodejs npm bind9-dnsutils ssl-cert
+apt-get install -y curl scons build-essential libssl-dev binutils-dev libleveldb-dev libsnappy-dev git nodejs npm bind9-dnsutils ssl-cert
 
-mkdir build
-cd build
+cd /tmp && mkdir build && cd build
 
 git clone https://github.com/cauldrondevelopmentllc/cbang
 git clone https://github.com/foldingathome/fah-node
@@ -15,5 +14,6 @@ scons -C cbang
 scons -C fah-node
 scons -C fah-node package
 
-dpkg --unpack fah-node/fah-node_$(cat fah-node/version.txt)_amd64.deb
+ls -la fah-node/*
 
+dpkg --unpack fah-node/fah-node_*_amd64.deb
--- fah-node/init.sh
+++ fah-node/init.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
-if [ ! -z $(ls /etc/fah-node/config.xml) ] && [ "${CONFIG_RECREATE}" == "false" ]; then
-    echo "There is already config file. Starting..."
+ARGS=""
+if [ ! -z "${NODE_ADMINS}" ]; then
+    ARGS="${ARGS} --admins ${NODE_ADMINS}"
 else
-    echo "Generating config file..."
-    generate_config()
+    echo "NODE_ADMINS is not set. Exiting..."
+    exit 1
 fi
+if [ ! -z "${NODE_DOMAINS}" ]; then
+    ARGS="${ARGS} --domains ${NODE_DOMAINS}"
+else
+    echo "NODE_DOMAINS is not set. Exiting..."
+    exit 1
+fi
+if [ ! -z "${NODE_GOOGLE_CLIENT_ID}" ]; then
+    ARGS="${ARGS} --google-client-id ${NODE_GOOGLE_CLIENT_ID}"
+else
+    echo "NODE_GOOGLE_CLIENT_ID is not set. Exiting..."
+    exit 1
+fi
+if [ ! -z "${NODE_GOOGLE_CLIENT_SECRET}" ]; then
+    ARGS="${ARGS} --google-client-secret ${NODE_GOOGLE_CLIENT_SECRET}"
+else
+    echo "NODE_GOOGLE_CLIENT_SECRET is not set. Exiting..."
+    exit 1
+fi
+if [ ! -z "${NODE_GOOGLE_REDIRECT_BASE}" ]; then
+    ARGS="${ARGS} --google-redirect-base ${NODE_GOOGLE_REDIRECT_BASE}"
+else
+    echo "NODE_GOOGLE_REDIRECT_BASE is not set. Exiting..."
+    exit 1
+fi
+if [ ! -z "${NODE_HTTP_ADDRESSES}" ]; then
+    ARGS="${ARGS} --http-addresses ${NODE_HTTP_ADDRESSES} --https-addresses ${NODE_HTTP_ADDRESSES}"
+else
+    ARGS="${ARGS} --http-addresses 0.0.0.0:80 --https-addresses 0.0.0.0:443a"
+fi
+
+echo "Starting fah-node..."
+/usr/bin/fah-node ${ARGS}
