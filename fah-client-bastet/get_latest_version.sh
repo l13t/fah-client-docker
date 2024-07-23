55dfea1bf4db3a7970ad34a369a499c9b8eac372
Dmytro Prokhorenkov <liet@liet.kiev.ua>
2024-07-23 08:55:33 +0200

    Changed way how to tag image from publi releases

---
+++ fah-client-bastet/get_latest_version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DEBIAN_FRONTEND="noninteractive" apt-get -y install jq curl
+
+ARCH=$(uname -m)
+
+case $ARCH in
+x86_64)
+    export ARCH_PATH="10-64bit"
+    ARCH="amd64"
+    ;;
+aarch64)
+    export ARCH_PATH="stable-arm64"
+    ARCH="arm64"
+    ;;
+*)
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+    ;;
+esac
+
+LATEST_VERSION=$(curl -s https://download.foldingathome.org/releases/public/fah-client/meta.json | jq -r '.[] | select((.package | contains("debian")) and (.package | contains("release")) and (.package | contains("'$ARCH_PATH'")) and (.package | contains(".deb"))) | .version | join(".")')
+
+echo "Building for $ARCH version $LATEST_VERSION"
+
+echo "LATEST_VERSION=$LATEST_VERSION" >>${GITHUB_ENV}
